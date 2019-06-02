82fc0c401b547368868ffd7419eaab4a318f1e6a
Thomas Yoho <TYohoJr@Gmail.com>
2019-06-02 12:25:03 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DataService } from 'src/services/data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HomeComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [DataService],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ src/services/data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  testApiService() {
+    let myHeaders = new HttpHeaders();
+    myHeaders.append('Authorization', 'Basic' + btoa('root:Festive9631'));
+    let apiResponse = this.http.get(
+      "http://192.168.0.10/api/v1.0/storage/volume",
+      // {
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'Authorization': 'Basic' + btoa('root:Festive9631'),
+      //     'Access-Control-Allow-Origin': '*'
+      //   }
+      // }
+    );
+    apiResponse.subscribe(data => {
+      console.log(data);
+    })
+  }
+}
